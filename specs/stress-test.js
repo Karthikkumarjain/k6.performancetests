00f8e092b50a2208564cc6b6203ec8d6d8402c6c
Karthik Kumar B <69575351+Karthikkumarjain@users.noreply.github.com>
2024-01-09 17:08:02 +0530

    Update stress-test.js

--- specs/stress-test.js
+++ specs/stress-test.js
@@ -58,3 +58,35 @@ export const options = {
         }
     ]
 }
+
+export default function () {
+ 
+    const url = ""; // put endpoint here
+ 
+    const payload = JSON.stringify(); // place json here ({})
+ 
+    //headers can be added here
+    const params = {
+        headers: {
+            'Content-Type': 'application/json',
+ 
+        },
+ 
+    };
+ 
+ 
+    const res = http.post(url, payload, params);
+ 
+    // TO add validations
+    check(res, {
+        'is status code as 200': (r) => r.status === 200,
+        'is res body': (r) => r.body.includes("J"),
+ 
+    });
+ 
+    // To log whole response
+    //console.log(res.body);
+ 
+    console.log(res.json("Data.0.Attribute")); // https://github.com/tidwall/gjson#path-syntax
+
+}
